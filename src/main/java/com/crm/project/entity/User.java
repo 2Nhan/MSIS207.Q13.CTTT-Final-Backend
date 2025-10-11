3107c3ac47be4e5ecc262cf1adca4ba71ea3e1f5
2Nhan <letunhan137@gmail.com>
2025-10-11 20:49:41 +0700

    Config db & port, create entity User

---
+++ src/main/java/com/crm/project/entity/User.java
@@ -0,0 +1,42 @@
+package com.crm.project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Column(length = 100, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", length = 15, unique = true)
+    private String phoneNumber;
+
+    @Column(length = 20)
+    private String address;
+
+    @Column(nullable = false)
+    private boolean active = true;
+}

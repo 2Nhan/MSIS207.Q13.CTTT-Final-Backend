d1cbb0acac456cf76e824715da063327645b3a05
2Nhan <letunhan137@gmail.com>
2025-10-12 11:40:34 +0700

    Implement jwt service with generate token functionality

---
+++ src/main/java/com/crm/project/service/JwtService.java
@@ -0,0 +1,50 @@
+package com.crm.project.service;
+
+import com.crm.project.entity.User;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.UUID;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.StringJoiner;
+
+@Service
+public class JwtService {
+    @Value("${jwt.key}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.access_duration}")
+    private int ACCESS_DURATION;
+
+    public String generateToken(User user) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .issuer("server")
+                .subject(user.getId())
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(ACCESS_DURATION, ChronoUnit.SECONDS).toEpochMilli()))
+                .jwtID(UUID.randomUUID().toString())
+//                .claim("scope", buildScope(user))
+                .build();
+
+        Payload payload = new Payload(claimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SECRET_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}

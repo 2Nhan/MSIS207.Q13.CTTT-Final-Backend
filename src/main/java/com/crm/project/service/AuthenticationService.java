c7f8564c96a3e75ed3b286272323dd4cb0a6d780
2Nhan <letunhan137@gmail.com>
2025-10-14 23:49:41 +0700

    Implement authentication service with login functionality

--- src/main/java/com/crm/project/service/AuthenticationService.java
+++ src/main/java/com/crm/project/service/AuthenticationService.java
@@ -1,4 +1,35 @@
 package com.crm.project.service;
 
+import com.crm.project.dto.request.LoginRequest;
+import com.crm.project.dto.response.AuthenticationResponse;
+import com.crm.project.entity.User;
+import com.crm.project.exception.AppException;
+import com.crm.project.exception.ErrorCode;
+import com.crm.project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        User user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND));
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        String token = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .authenticated(true)
+                .accessToken(token)
+                .build();
+    }
 }

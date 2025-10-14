9dd96fb584ce083dc2d10b3148aa163bd6d10cdb
2Nhan <letunhan137@gmail.com>
2025-10-14 23:12:56 +0700

    Override JwtDecoder to a new custom one

--- src/main/java/com/crm/project/configuration/JwtDecoderConfiguration.java
+++ src/main/java/com/crm/project/configuration/JwtDecoderConfiguration.java
@@ -1,4 +1,50 @@
 package com.crm.project.configuration;
 
-public class JwtDecoderConfiguration {
+import com.crm.project.service.JwtService;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Objects;
+
+import java.io.Serializable;
+import java.text.ParseException;
+
+@Component
+public class JwtDecoderConfiguration implements JwtDecoder {
+    @Value("${jwt.key}")
+    private String secretKey;
+
+    private NimbusJwtDecoder nimbusJwtDecoder = null;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    public Jwt decode(String token) throws JwtException {
+
+        try {
+            boolean isValid = jwtService.verifyToken(token, secretKey);
+            if (!isValid) {
+                throw new JwtException("Invalid token")
+            }
+        } catch (JOSEException | ParseException e) {
+            throw new JwtException("Invalid token");
+        }
+
+        if(Objects.isNull(nimbusJwtDecoder)) {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HS256");
+            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+        }
+
+        return nimbusJwtDecoder.decode(token);
+    }
 }

f83a1ba0c6f9d514889d3965bf3d6f4dea4ff093
2Nhan <letunhan137@gmail.com>
2025-11-23 16:40:36 +0700

    Optimize query

--- src/main/java/com/crm/project/repository/LeadRepository.java
+++ src/main/java/com/crm/project/repository/LeadRepository.java
@@ -3,6 +3,7 @@ package com.crm.project.repository;
 
 import com.crm.project.entity.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -21,5 +22,9 @@ public interface LeadRepository extends JpaRepository<Lead, String> {
                 WHERE l.id = :id
             """)
     Optional<Lead> findByIdWithRelations(@Param("id") String id);
+
+    @Modifying
+    @Query("UPDATE Lead l SET l.stage.id = :stageId WHERE l.id = :leadId")
+    int updateStage(@Param("leadId") String leadId, @Param("stageId") String stageId);
 
 }

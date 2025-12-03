d3ffd74f5a0f6cab1ccc017c07dc413204938e7b
2Nhan <letunhan137@gmail.com>
2025-12-03 10:41:33 +0700

    Quotation mail format

---
+++ src/main/java/com/crm/project/internal/LeadQuotationInfo.java
@@ -0,0 +1,4 @@
+package com.crm.project.internal;
+
+public class LeadQuotationInfo {
+}
--- src/main/java/com/crm/project/service/MailService.java
+++ src/main/java/com/crm/project/service/MailService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MailService {
@@ -45,7 +47,25 @@ public class MailService {
         }
     }
 
-    public void sendQuotationMail(String to) {
+    public void sendQuotationMail(String to, String fullName, List<String> products, Byte[] attachments) {
+        String subject = "Báo giá cho danh mục sản phẩm: " + products.toString() + ",";
+        String text = "Kính gửi: Anh/Chị " + fullName
+                + "\nLời đầu tiên, chúng tôi xin cảm ơn Quý khách hàng đã quan tâm đến sản phẩm của chúng tôi."
+                + "\nTiếp nhận yêu cầu của Anh/Chị, tôi xin gửi kèm email này bảng báo giá chi tiết cho hạng mục"
+                + products.toString()
+                + "Trong file đính kèm, chúng tôi đã liệt kê rõ các thông số kỹ thuật, đơn giá và các chính sách ưu đãi dành riêng cho Quý khách hàng trong tháng này."
+                + "\nAnh/Chị vui lòng xem file PDF đính kèm để có thông tin đầy đủ nhất."
+                + "\nMong sớm nhận được phản hồi từ Anh/Chị"
+                + "\nTrân trọng,"
+                + "\nNhân";
+
+        CreateEmailOptions email = CreateEmailOptions.builder()
+                .from(sender)
+                .to(to)
+                .subject(subject)
+                .text(text)
+//                .attachments()
+                .build();
     }
 
 
